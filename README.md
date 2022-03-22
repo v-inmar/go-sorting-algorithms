0585bc7e98f9774e55eda6e5bb7b01f5c42ebb48
Vinmar Tipay <vinmar.tipay@outlook.com>
2022-03-22 20:39:38 +0000

    added insertion sort

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Simple implementations of different sorting algorithms written in Go programming language
 
 - [x] **Selection Sort**
-- [ ] **Insertion Sort**
+- [x] **Insertion Sort**
 - [ ] **Bubble Sort**
 - [ ] **Merge Sort**
 - [ ] **Quick Sort**
--- main.go
+++ main.go
@@ -5,9 +5,24 @@ import (
 )
 
 func main(){
+
+	// Selection
+	fmt.Println("#----# Selection #----#")
 	toSort := []int{345,2,756,723,34,12,45,67,678,567,234,894,904,324,4536,456}
 	fmt.Printf("Length: %d Unsorted Values: %v\n", len(toSort), toSort)
 
 	toSort = selection(toSort)
 	fmt.Printf("Length: %d Unsorted Values: %v\n", len(toSort), toSort)
+	fmt.Println("#---------------------#")
+
+	fmt.Println()
+	
+	// Insertion
+	fmt.Println("#----# Insertion #----#")
+	toSort = []int{345,2,756,723,34,12,45,67,678,567,234,894,904,324,4536,456}
+	fmt.Printf("Length: %d Unsorted Values: %v\n", len(toSort), toSort)
+
+	toSort = insertion(toSort)
+	fmt.Printf("Length: %d Unsorted Values: %v\n", len(toSort), toSort)
+	fmt.Println("#---------------------#")
 }
